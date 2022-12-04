85868e28504ba4731b924adf64e1b98c0d87c339
dr7ocino <morochiste@gmail.com>
2022-12-03 21:47:26 -0400

    se agrego la funcion de crear archivo

--- I_O/exercise1.py
+++ I_O/exercise1.py
@@ -3,11 +3,18 @@ class Archivos:
     path =''
     LECTURA='r'
     
-    def __init__(self,path):
+    def __init__(self,path,file_name):
         self.path=path
         self.ESCRITURA='r'
         self.LECTURA='a'
-    
+        self.file_name=file_name
+    def createFile(self):
+        try:
+            with open(self.file_name, self.LECTURA) as f:
+                f.write('Create a new text file')
+        except FileNotFoundError:
+            print("The directory doesn't exist") 
+
     def lectura(self):
         f=open(self.path, self.LECTURA)
         data=f.readlines()

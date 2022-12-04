cec3b5abe8cd0e0625a2ca2726f67cf4165c471d
dr7ocino <morochiste@gmail.com>
2022-12-03 22:45:32 -0400

    se implemento la funcion de escriturade archivo

--- I_O/exercise1.py
+++ I_O/exercise1.py
@@ -3,33 +3,37 @@ class Archivos:
     path =''
     LECTURA='r'
     
-    def __init__(self,path,file_name):
-        self.path=path
-        self.ESCRITURA='r'
-        self.LECTURA='a'
+    def __init__(self,file_name):
+        #self.path=path
+        self.ESCRITURA='a'
+        self.LECTURA='r'
         self.file_name=file_name
     def createFile(self):
         try:
-            with open(self.file_name, self.LECTURA) as f:
-                f.write('Create a new text file')
+            with open(self.file_name, 'w') as f:
+                f.write('Welcome to my text file\n')
+                print(f'Create a new file: {self.file_name}\n')
         except FileNotFoundError:
             print("The directory doesn't exist") 
 
     def lectura(self):
-        f=open(self.path, self.LECTURA)
+        f=open(self.file_name, self.LECTURA)
         data=f.readlines()
         f.close()
         return data 
         
-    def escritura():
-    
-        pass 
+    def escritura(self):
+         f=open(self.file_name,self.ESCRITURA)
+         f.write("I'm writting a line\n")
 
 
 def main():
-    path = "/etc/passwd"
+    #path = "/etc/passwd"
     name_file='test.txt'
-    data=Archivos(path=path,file_name=name_file)
+    data=Archivos(file_name=name_file)
+    #datos=data.lectura()
+    data.createFile()
+    data.escritura()
     datos=data.lectura()
     for read in datos:
         print (read)
